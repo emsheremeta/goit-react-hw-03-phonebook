f3176304ddc9a41eb9d4c3813f95f446590d29d4
Marina <sheremetamaryna@gmail.com>
2022-11-06 22:51:22 +0100

    upd

--- src/components/PhoneApp.jsx
+++ src/components/PhoneApp.jsx
@@ -1,94 +1,87 @@
-import React from "react";
-import ContactFilter from "./phonebook/ContactFilter";
-import Form from "./phonebook/Form"
-import {nanoid} from "nanoid"
-import "./phonebook/Phonebook.css"
+import React from 'react';
+import ContactFilter from './phonebook/ContactFilter';
+import Form from './phonebook/Form';
+import { nanoid } from 'nanoid';
+import './phonebook/Phonebook.css';
 
+import ContactList from './phonebook/ContactList';
 
-import ContactList from "./phonebook/ContactList";
-
-class PhoneApp extends React.Component { 
-  
+class PhoneApp extends React.Component {
   state = {
     contacts: [],
-    filter: ''
+    filter: '',
   };
 
-  componentDidMount () {
-    console.log ('did mount')
+  componentDidMount() {
+    console.log('did mount');
 
     const contacts = JSON.parse(localStorage.getItem('contacts'));
     if (contacts) {
-      this.setState({contacts : contacts});
+      this.setState({ contacts: contacts });
     }
   }
 
-  componentDidUpdate (prevProps, prevState) {
-    console.log('did update')
-    if (this.state.contacts !== prevState.contacts){
-     console.log ( 'upd');
+  componentDidUpdate(prevProps, prevState) {
+    console.log('did update');
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('upd');
 
-     localStorage.setItem ('contacts', JSON.stringify(this.state.contacts));
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
 
-  onChange = (event) => {
-    this.setState ({filter : event.currentTarget.value});
-  }
-  onSubmit = (data) => {
+  onChange = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+  onSubmit = data => {
     console.log(data);
-    const {name, number} = data;
-    console.log(this.state.contacts.filter(contact=>contact.name===name).length);
+    const { name, number } = data;
 
-    if (this.state.contacts.filter(contact=>contact.name.toLocaleLowerCase()===name.toLocaleLowerCase()).length===0) {
+    if (
+      this.state.contacts.filter(
+        contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+      ).length === 0
+    ) {
       const id = nanoid();
       const contact = {
         id: id,
         name: name,
-        number: number
+        number: number,
       };
       this.setState(prevState => ({
-        contacts: [...prevState.contacts, contact]
-    }))
+        contacts: [...prevState.contacts, contact],
+      }));
     } else {
-     alert('This contact already exist')
+      alert('This contact already exist');
     }
-    
-  }
+  };
 
-  onDelete = (event) =>{
+  onDelete = event => {
     const id = event.target.parentElement.id;
     this.setState({
-     contacts:this.state.contacts.filter(contact=>contact.id!==id)
-    })
-     
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+  render() {
+    return (
+      <div>
+        <h1 className="Phonebook__text">Phonebook</h1>
+
+        <Form onSubmit={this.onSubmit}></Form>
+
+        <h2 className="Phonebook__text__contacts">Contacts</h2>
+
+        <ContactFilter
+          filter={this.state.filter}
+          onChange={this.onChange}
+        ></ContactFilter>
+        <ContactList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onDelete={this.onDelete}
+        ></ContactList>
+      </div>
+    );
   }
-  render () {
-   return (
-    <div>
-    <h1 className="Phonebook__text">Phonebook</h1>
-     
-      <Form
-      onSubmit = {this.onSubmit}>
-      </Form>
-
-      <h2 className="Phonebook__text__contacts">Contacts</h2>
-
-      <ContactFilter
-      filter = {this.state.filter}
-      onChange = {this.onChange}
-      ></ContactFilter>
-      <ContactList
-      contacts = {this.state.contacts}
-      filter={this.state.filter}
-      onDelete ={this.onDelete}> 
-      </ContactList>
-           
-                 
-    </div>
-    
-  );
-    }
-
-};
+}
 export default PhoneApp;
